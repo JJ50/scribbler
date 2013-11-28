a20a7cdec51b40916f933d5553caf21515bf47d9
inomani <inomani@college.harvard.edu>
2013-11-27 20:41:24 -0500

    Create constants.php

---
+++ constants.php
@@ -0,0 +1,24 @@
+<?php
+
+    /**
+     * constants.php
+     *
+     * Computer Science 50
+     * myScribbler
+     *
+     * Global constants.
+     */
+
+    // your database's name
+    define("DATABASE", "myscribbler_db");
+
+    // your database's password
+    define("PASSWORD", "ir6ErRbj");
+
+    // your database's server
+    define("SERVER", "mysql.myscribbler.us");
+
+    // your database's username
+    define("USERNAME", "myscribbler13");
+
+?>
