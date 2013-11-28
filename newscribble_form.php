b49603a0526a9481efa883341f90083e44d91c32
inomani <inomani@college.harvard.edu>
2013-11-28 16:54:05 -0500

    Create newscribble_form.php

---
+++ newscribble_form.php
@@ -0,0 +1,14 @@
+<form action="newscribble.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus name="scribbles" placeholder="Enter Today's Scribble" type="text"/>
+        </div>
+        
+        <div class="form-group">
+            <button type="submit" class="btn">Scribble It</button>
+        </div>
+    </fieldset>
+</form>
+<div>
+    <a href="logout.php">Log Out</a>
+</div>
