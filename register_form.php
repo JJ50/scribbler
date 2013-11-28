7e47fefb98d0964697921bfe5356bb3bf5894bdd
inomani <inomani@college.harvard.edu>
2013-11-27 23:27:30 -0500

    Update register_form.php

--- register_form.php
+++ register_form.php
@@ -1,19 +1,19 @@
 <form action="register.php" method="post">
     <fieldset>
         <div class="form-group">
-            <input autofocus class="form-control" name="username" placeholder="Username" type="text"/>
+            <input autofocus name="username" placeholder="Enter Username" type="text"/>
         </div>
         <div class="form-group">
-            <input class="form-control" name="password" placeholder="Password" type="password"/>
+            <input name="password" placeholder="Enter Password" type="password"/>
         </div>
         <div class="form-group">
-            <input class="form-control" name="confirmation" placeholder="Password (again)" type="password"/>
+            <input name="confirmation" placeholder="Enter Password Again" type="password"/>
         </div>
         <div class="form-group">
-            <button type="submit" class="btn">Register</button>
+            <button type="submit" class="btn btn-default">Register!</button>
         </div>
     </fieldset>
 </form>
 <div>
-    or <a href="login.php">log in</a>
+    or <a href="login.php">sign in</a>
 </div>
