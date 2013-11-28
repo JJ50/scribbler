52782c00575e4ef0b5177a3d6ad3f8620f727660
inomani <inomani@college.harvard.edu>
2013-11-28 15:00:40 -0500

    Update and rename portfolio.php to main.php

---
+++ main.php
@@ -0,0 +1,12 @@
+<ul class="nav nav-pills">
+    <li><a href="allscribbles.php">All Scribbles</a></li>
+    <li><a href="statistics.php">Statistics</a></li>
+    <li><a href="logout.php"><strong>Log Out</strong></a></li>
+</ul>
+
+<div>
+    <img alt="Under Construction" src="/img/logo.gif"/>
+</div>
+<div>
+    <a href="logout.php">Log Out</a>
+</div>
