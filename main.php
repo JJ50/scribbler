a9e37c3ed4518880f4502794fd8317d76aaa3e2f
inomani <inomani@college.harvard.edu>
2013-11-28 16:46:50 -0500

    Update main.php

--- main.php
+++ main.php
@@ -1,6 +1,7 @@
 <ul class="nav nav-pills">
     <li><a href="allscribbles.php">All Scribbles</a></li>
     <li><a href="statistics.php">Statistics</a></li>
+    <li><a href="newscribble.php">Scribble Something!</a></li>
     <li><a href="logout.php"><strong>Log Out</strong></a></li>
 </ul>
 
