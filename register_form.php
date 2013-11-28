4068d11c27bc2c1bc8fe930d64a85e2210ee2dee
inomani <inomani@college.harvard.edu>
2013-11-28 15:34:00 -0500

    Update register_form.php

--- register_form.php
+++ register_form.php
@@ -1,6 +1,12 @@
 <form action="register.php" method="post">
     <fieldset>
         <div class="form-group">
+            <input autofocus name="first" placeholder="Enter First Name" type="text"/>
+        </div>
+        <div class="form-group">
+            <input autofocus name="last" placeholder="Enter Last Name" type="text"/>
+        </div>
+        <div class="form-group">
             <input autofocus name="username" placeholder="Enter Username" type="text"/>
         </div>
         <div class="form-group">
@@ -8,6 +14,9 @@
         </div>
         <div class="form-group">
             <input name="confirmation" placeholder="Enter Password Again" type="password"/>
+        </div>
+        <div class="form-group">
+            <input autofocus name="email" placeholder="Enter Your Email Address" type="text"/>
         </div>
         <div class="form-group">
             <button type="submit" class="btn btn-default">Register!</button>
