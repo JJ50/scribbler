884bf70003b5482c82d3689dbd85a7a1345f43e0
inomani <inomani@college.harvard.edu>
2013-11-29 17:36:10 -0500

    Update newscribble.php

--- newscribble.php
+++ newscribble.php
@@ -3,8 +3,26 @@
     // configuration
     require("../includes/config.php"); 
     
-    
-    // render portfolio
-    render("newscribble_form.php");
+    // if form was submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {    
+         // validate input
+         if (empty($_POST["scribble"]))
+         {
+             apologize("No Scribble entered!");
+         }   
+         
+         // try to store scribble 
+          query("INSERT INTO scribbles (id, scribbletext, datetime) VALUES (?, ?, NOW())", $_SESSION["id"], $_POST["scribble"]);
 
+        // redirect to main page
+        render("newscribble_form.php", ["title" => "Scribble Something!"]);
+
+    }
+ 
+    // render portfolio
+    else
+    {
+    render("newscribble_form.php", ["title" => "Scribble Something!"]);
+    }
 ?>
